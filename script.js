63eddff8cf3fbaf4a637dfce7cbcc3fbb9e786db
minscoid-ship-it <mins.co.id@gmail.com>
2025-12-31 01:38:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ e.preventDefault();
 const d=new FormData(e.target);
 let m='FORM BRIEF MINS MEDIA\n\n';
 d.forEach((v,k)=>m+=k+': '+v+'\n');
-window.open('https://wa.me/6285794552908?text='+encodeURIComponent(m));
+window.open('https://wa.me/6283872106321?text='+encodeURIComponent(m));
 };
