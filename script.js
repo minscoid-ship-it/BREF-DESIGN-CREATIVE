2878f53738593b4d6eb391b20bfacf9f9e3e2ae7
minscoid-ship-it <mins.co.id@gmail.com>
2025-12-31 01:34:50 +0700

    Update WhatsApp link in script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ e.preventDefault();
 const d=new FormData(e.target);
 let m='FORM BRIEF MINS MEDIA\n\n';
 d.forEach((v,k)=>m+=k+': '+v+'\n');
-window.open('https://wa.me/6281234567890?text='+encodeURIComponent(m));
+window.open('https://wa.me/6285794552908?text='+encodeURIComponent(m));
 };
